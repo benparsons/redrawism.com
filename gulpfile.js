54b537fc038ed962c7c7105e9619f432e27686a9
Slavomir Vojacek <slavomir@adbrain.com>
2014-12-11 17:33:15 +0000

    clean styling, code refactoring, implement litchi fully, for the way this website was created is too slapdash

--- gulpfile.js
+++ gulpfile.js
@@ -22,42 +22,15 @@ gulp.task("sass", function() {
 			loadPath: process.cwd() + "/css/include",
 			style: "nested"
 		}))
-		.pipe(autoprefixer("last 2 version", "> 1%"))
+		.pipe(autoprefixer("last 20 version", "> 1%"))
 		.pipe(gulp.dest("css"))
 		.pipe(notify("SASS successfully compiled!"));
-
-});
-
-gulp.task("sass-litchi", function() {
-
-	gulp.src("css/include/litchi.sass")
-		.pipe(sass({
-			loadPath: process.cwd() + "/css/include",
-			style: "nested"
-		}))
-		.pipe(autoprefixer("last 2 version", "> 1%"))
-		.pipe(gulp.dest("css/export"))
-		.pipe(rename({suffix: ".min"}))
-		.pipe(minifycss())
-		.pipe(gulp.dest("css/export"))
-		.pipe(notify("SASS Litchi successfully compiled!"));
-
-});
-
-gulp.task("uglify", function() {
-
-	gulp.src("js/include/*.js")
-		.pipe(concat("app.js"))
-		.pipe(uglify("app.js"))
-		.pipe(gulp.dest("js"))
-		.pipe(notify("JavaScript successfully compiled!"));
 
 });
 
 gulp.task("watch", function() {
 
 	gulp.watch("css/include/**/*.sass", ["sass"]);
-	gulp.watch("js/include/*.js", ["uglify"]);
 
 });
 
