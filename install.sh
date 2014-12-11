4de873f0de404c620d108f5dc486fea7845068bd
Slavomir Vojacek <slavomir@adbrain.com>
2014-12-11 19:32:57 +0000

    added install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+ # Install Node.js and Ruby
+sudo apt-get install nodejs
+sudo apt-get install ruby
+
+# Install sass
+sudo gem install Sass
+
+# Setup gulp
+sudo npm install gulp -g
+sudo npm install gulp
+sudo npm install gulp-ruby-sass gulp-minify-css gulp-rename gulp-autoprefixer gulp-uglify gulp-concat gulp-notify
+
+# Start gulp monitoring of the sass files
+gulp
